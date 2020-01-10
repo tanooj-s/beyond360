fbe9cd96b4d114f5a0458c45fd50fbed7572ddb6
Tanooj Shah <tanooj.s.shah@gmail.com>
2020-01-10 20:13:28 +0530

    added review text, messed with mailer settings

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -8,6 +8,6 @@ class MessageMailer < ApplicationMailer
   def contact_us(message)
     @body = message.body
 
-    mail to: "beyond360@gmail.com", from: message.email
+    mail to: "ojtano@gmail.com", from: message.email
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,17 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = {host: host}
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "ojtano",
+    :password => "Bumbaberk#*~",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -58,4 +69,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
